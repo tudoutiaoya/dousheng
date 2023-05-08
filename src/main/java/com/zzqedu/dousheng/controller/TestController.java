59c512d40af43d1af6473cd70a2cf8b4e06279fc
zuzaiquan <zzq1292748978@163.com>
2023-05-08 14:54:20 +0800

    add: 测试actions

--- src/main/java/com/zzqedu/dousheng/controller/TestController.java
+++ src/main/java/com/zzqedu/dousheng/controller/TestController.java
@@ -5,12 +5,15 @@ import com.zzqedu.dousheng.core.common.resp.RestResp;
 import com.zzqedu.dousheng.service.UserService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@Slf4j
 @Tag(name = "test", description = "测试服务")
 @RequiredArgsConstructor
 @RestController
@@ -27,6 +30,7 @@ public class TestController {
 
     @GetMapping("/hello")
     public RestResp hello() {
+        log.info("测试hello");
         return RestResp.builder().withToken("hello").build();
     }
 
